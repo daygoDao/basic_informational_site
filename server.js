1de0f3f7b487a2a25ae5686135f6572ad321ddab
dao vo <gary.vo32@gmail.com>
2022-05-23 19:52:52 -0700

    set views to play wit h ejs

--- server.js
+++ server.js
@@ -5,63 +5,78 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  console.log('here');
-  res.status(200).json({message: "error"});
-})
-app.listen(3000);
+app.set('views', path.join(__dirname, '/views'))
 
 console.log("yo from app.js");
 const hostname = "127.0.0.1";
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  const { header, method, url } = req;
-  console.log(url);
+app.get('/', (req, res) => {
+  console.log('here');
+  res.status(200).render('home');
+})
 
-  if (req.url === "/") {
-    fs.readFile(
-      path.join(__dirname, "./", "index.html"),
-      "utf8",
-      (err, data) => {
-        if (err) throw err;
+app.get('/about', (req, res) => {
+  console.log('here');
+  res.status(200).send('this is about');
+})
 
-        console.log(data);
+app.get('/contact', (req, res) => {
+  console.log('here');
+  res.status(200).send('this is contact');
+})
 
-        res.write(data);
-        res.end();
-      }
-    );
-  } else if (req.url === "/about") {
-    fs.readFile(
-      path.join(__dirname, "./", "about.html"),
-      "utf8",
-      (err, data) => {
-        if (err) throw err;
-
-        console.log(data);
-
-        res.write(data);
-        res.end();
-      }
-    );
-  } else {
-    fs.readFile(
-      path.join(__dirname, "./", "404.html"),
-      "utf8",
-      (err, data) => {
-        if (err) throw err;
-
-        console.log(data);
-
-        res.write(data);
-        res.end();
-      }
-    );
-  }
+app.listen(port, () => {
+  console.log(`server is listening on port: ${port}`)
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+// const server = http.createServer((req, res) => {
+//   const { header, method, url } = req;
+//   console.log(url);
+
+//   if (req.url === "/") {
+//     fs.readFile(
+//       path.join(__dirname, "./", "index.html"),
+//       "utf8",
+//       (err, data) => {
+//         if (err) throw err;
+
+//         console.log(data);
+
+//         res.write(data);
+//         res.end();
+//       }
+//     );
+//   } else if (req.url === "/about") {
+//     fs.readFile(
+//       path.join(__dirname, "./", "about.html"),
+//       "utf8",
+//       (err, data) => {
+//         if (err) throw err;
+
+//         console.log(data);
+
+//         res.write(data);
+//         res.end();
+//       }
+//     );
+//   } else {
+//     fs.readFile(
+//       path.join(__dirname, "./", "404.html"),
+//       "utf8",
+//       (err, data) => {
+//         if (err) throw err;
+
+//         console.log(data);
+
+//         res.write(data);
+//         res.end();
+//       }
+//     );
+//   }
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
